6e2e62ba03e5f3a1d7de3ecbb2041fbb65f5af96
Chandra Dev <yadavchandradev2000@gmail.com>
2024-11-05 19:25:35 +0545

    footer changed

--- src/Components/Footer/CopyRight.jsx
+++ src/Components/Footer/CopyRight.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const CopyRight = () => {
   return (
     <div className='flex justify-center items-center py-12'>
-        <p className='text-lg font-medium text-center'>All Rights Reserved © MilanBindu || Design By: Chandra Dev</p>
+        <p className='text-sm md:text-lg font-medium text-center'>All Rights Reserved © MilanBindu || Design By: Chandra Dev</p>
     </div>
   )
 }
